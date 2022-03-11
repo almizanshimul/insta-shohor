415af695f3987c574b8c6d6802a1a20ad1bb9887
Al Mizan <67356360+almizanshimul@users.noreply.github.com>
2022-03-11 19:15:17 +0600

    question-and-answer hide and show

--- js/app.js
+++ js/app.js
@@ -45,18 +45,21 @@ const displayContent = (text) => {
 const switchTab = (id) => {
   if (id === "posts") {
     document.getElementById("posts").style.display = "grid";
+    document.getElementById("question-and-answer").style.display = "block";
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById("question-and-answer").style.display = "none";
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+    document.getElementById("question-and-answer").style.display = "none";
 
     displayReportedPosts();
   }
